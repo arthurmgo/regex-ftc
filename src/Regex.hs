40d0953f0270daab183cd6a209f749495e072c28
Arthur Miranda Gomes <arthur-m-gomes@hotmail.com>
2017-06-29 02:09:04 -0300

    update regex

--- src/Regex.hs
+++ src/Regex.hs
@@ -17,5 +17,27 @@ printRePol (Union r1 r2)  = printRePol r1 ++ printRePol r2 ++ "+"
 printRePol (Concat r1 r2) = printRePol r1 ++ printRePol r2 ++ "."
 printRePol (Kleene r)     = printRePol r ++ "*"
 
+printRe :: Regex -> String
+printRe Lambda         = "@"
+printRe (Lit a)        = [a]
+printRe (Union r1 r2)  = "(" ++ printRe r1 ++ "+" ++ printRe r2 ++ ")"
+printRe (Concat r1 r2) = "(" ++ printRe r1 ++ printRe r2 ++ ")"
+printRe (Kleene r)     = "(" ++ printRe r ++ ")*"
+
+charToString :: Char -> String
+charToString c = [c]
+
+chars :: String -> [String]
+chars = map charToString
+
 str2regex :: String -> Regex
-str2regex = undefined
+str2regex = head . foldl foldFunction [] . chars
+    where
+        foldFunction (x:ys) "*" = Kleene x : ys
+        foldFunction (x:y:ys) "+" = Union y x : ys
+        foldFunction (x:y:ys) "." = Concat y x : ys
+        foldFunction xs [a] = Lit a : xs
+
+
+instance Show Regex where
+  show = printRe
