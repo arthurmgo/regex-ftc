667f3440e038dec42bc80a20b4c68fa0fad72e12
Arthur Miranda Gomes <arthur-m-gomes@hotmail.com>
2017-06-30 02:55:21 -0300

    update

--- src/MenuIO.hs
+++ src/MenuIO.hs
@@ -2,5 +2,23 @@ module MenuIO
     ( menu
     ) where
 
-menu :: IO ()
-menu = putStrLn "someFunc"
+import           Regex
+
+type Tag = (String, String)
+
+split :: String -> (String, String)
+split = span (== ' ')
+
+menu :: [Tag] -> IO ()
+menu tag = do
+  str <- getLine
+  let op    = take 2 str
+      file  = drop 3 str
+  case op of
+    ":l"      -> putStrLn "FIM DO PROGRAMA"
+    ":f"      -> putStrLn "FIM DO PROGRAMA"
+    ":o"      -> putStrLn "FIM DO PROGRAMA"
+    ":p"      -> putStrLn "FIM DO PROGRAMA"
+    ":q"      -> putStrLn "FIM DO PROGRAMA"
+    ":s"      -> putStrLn "FIM DO PROGRAMA"
+    _         -> return ()
