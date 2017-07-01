644ba4312b399d229d98a180ff01f9b24a5f65bd
Arthur Miranda Gomes <arthur-m-gomes@hotmail.com>
2017-06-30 22:29:55 -0300

    update menu

--- src/MenuIO.hs
+++ src/MenuIO.hs
@@ -2,12 +2,10 @@ module MenuIO
     ( menu
     ) where
 
+import           Data.List.Split
 import           Regex
 
 type Tag = (String, String)
-
-split :: String -> (String, String)
-split = span (== ' ')
 
 menu :: [Tag] -> IO ()
 menu tag = do
@@ -15,10 +13,19 @@ menu tag = do
   let op    = take 2 str
       file  = drop 3 str
   case op of
-    ":l"      -> putStrLn "FIM DO PROGRAMA"
-    ":f"      -> putStrLn "FIM DO PROGRAMA"
-    ":o"      -> putStrLn "FIM DO PROGRAMA"
-    ":p"      -> putStrLn "FIM DO PROGRAMA"
-    ":q"      -> putStrLn "FIM DO PROGRAMA"
-    ":s"      -> putStrLn "FIM"
-    _         -> return ()
+    ":l" -> putStrLn "FIM DO PROGRAMA"
+    ":f" -> putStrLn "FIM DO PROGRAMA"
+    ":o" -> putStrLn "FIM DO PROGRAMA"
+    ":p" -> putStrLn "FIM DO PROGRAMA"
+    ":q" -> putStrLn "FIM DO PROGRAMA"
+    ":s" -> putStrLn "FIM"
+    _    -> return ()
+
+
+splitTag :: String -> String -> (String, String)
+splitTag str s = (tag,expr)
+  where
+    spl  = splitOn s str
+    tag  = head spl
+    len  = length $ s ++ tag
+    expr = drop len str
