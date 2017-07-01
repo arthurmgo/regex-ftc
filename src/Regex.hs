cd3c15895ed536a96a44c2346c9b354c4fc6b9d9
Arthur Miranda Gomes <arthur-m-gomes@hotmail.com>
2017-07-01 18:54:22 -0300

    update

--- src/Regex.hs
+++ src/Regex.hs
@@ -36,12 +36,19 @@ chars = map charToString
 
 
 str2regex :: String -> Regex
-str2regex = head . foldl foldFunction [] . chars
+str2regex = head . foldl foldFunction [] . str2token
     where
-        foldFunction (x:ys)   "*" = Kleene x : ys
-        foldFunction (x:y:ys) "+" = Union y x : ys
-        foldFunction (x:y:ys) "." = Concat y x : ys
-        foldFunction    xs    [a] = Lit a : xs
+        foldFunction (x:ys)   (TChar '*') = Kleene x   : ys
+        foldFunction (x:y:ys) (TChar '+') = Union y x  : ys
+        foldFunction (x:y:ys) (TChar '.') = Concat y x : ys
+        foldFunction    xs    TPlus       = Lit '+'    : xs
+        foldFunction    xs    TMinus      = Lit '-'    : xs
+        foldFunction    xs    TTimes      = Lit '*'    : xs
+        foldFunction    xs    TBar        = Lit '\\'   : xs
+        foldFunction    xs    TEnter      = Lit '\n'   : xs
+        foldFunction    xs    TLambda     = Lambda     : xs
+        foldFunction    xs    TDot        = Lit '.'    : xs
+        foldFunction    xs    (TChar a)   = Lit a      : xs
 
 
 instance Show Regex where
