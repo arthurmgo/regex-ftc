093e00f3aec4687a1f86f1e940bccfe81dd3afe9
arthurmgo <arthur-m-gomes@hotmail.com>
2017-08-20 18:17:27 -0300

    update

--- src/BuildNfa.hs
+++ src/BuildNfa.hs
@@ -28,12 +28,47 @@ mOr (NFA states1 moves1 start1 finish1) (NFA states2 moves2 start2 finish2)
      states1' = mapSet (renumber 1) states1
      states2' = mapSet (renumber (m1 + 1)) states2
      newstates = makeSet [0, m1 + m2 + 1]
-     moves1' = mapSet (renumber_move 1) moves1
-     moves2' = mapSet (renumber_move (m1 + 1)) moves2
+     moves1' = mapSet (renumberMove 1) moves1
+     moves2' = mapSet (renumberMove (m1 + 1)) moves2
      newmoves = makeSet [Emove 0 1, Emove 0 (m1+1), Emove m1 (m1 + m2+1), Emove (m1+m2) (m1+m2+1)]
 
+-- Até aqui OK!
+
 mThen :: Nfa Int -> Nfa Int -> Nfa Int
-mThen = undefined
+mThen (NFA states1 moves1 start1 finish1) (NFA states2 moves2 start2 finish2)
+  = NFA (states1 `union` states2') (moves1 `union` moves2')
+     	start1
+     	finish2'
+
+     	where
+
+     	states2' = mapSet (renumber k) states2
+     	moves2'  = mapSet (renumberMove k) moves2
+     	finish2' = mapSet (renumber k) finish2
+     	k = card states1 - 1
 
 mStar :: Nfa Int -> Nfa Int
-mStar = undefined
+mStar (NFA states moves start finish)
+    = NFA
+      (states' `union` newstates)
+      (moves' `union` newmoves)
+       0
+      (sing (m+1))
+        where
+          m = card states
+          states' = mapSet (renumber 1) states
+          newstates = makeSet [ 0 , m+1 ]
+          moves'  = mapSet (renumberMove 1) moves
+          newmoves = makeSet [ Emove 0 1 , Emove m 1 , Emove 0 (m+1) , Emove m (m+1) ]
+
+
+
+renumber :: Int -> Int -> Int
+renumber n st = n + st
+
+renumberMove :: Int -> Move Int -> Move Int
+renumberMove k (Move s1 c s2)
+    = Move (renumber k s1) c (renumber k s2)
+
+renumberMove k (Emove s1 s2)
+    = Emove (renumber k s1) (renumber k s2)
